54159c5f30ecfed3a375c66d8ae321f8af0b1f8c
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-06-09 13:00:22 +0300

    Create basic Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,54 @@
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import "./Header.scss"
+import classnames from "classnames"
+import Logo from "../../assets/apelvil-origin.svg"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+function Header() {
+  const [changed, setChanged] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, [])
+
+  function handleScroll() {
+    const isChanged = window.pageYOffset > 0
+    setChanged(isChanged)
+  }
+
+  return (
+    <header
+      className={classnames("header", {
+        "header--changed": changed,
+      })}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+      <div className="header__logo">
+        <Link to="/" className="header__logo--link">
+          <Logo />
         </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      </div>
+      <nav
+        className={classnames("nav", {
+          "nav--changed": changed,
+        })}
+      >
+        <div className="nav__item">
+          <span>BAS</span>
+        </div>
+        <div className="nav__item">
+          <span>1С:Підприємство</span>
+        </div>
+        <div className="nav__item">Пактум</div>
+        <div className="nav__item">Послуги</div>
+        <div className="nav__item">Про нас</div>
+        <div className="nav__item">Контакты</div>
+      </nav>
+      <div className="header__menu-button">
+        <span>меню</span>
+        <div className="circle"></div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
